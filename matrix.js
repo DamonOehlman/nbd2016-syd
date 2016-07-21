cecc46dbc22c904cb8a284c1ade46258359ff978
Damon Oehlman <damon.oehlman@gmail.com>
2016-07-21 15:45:58 +1000

    added simple matrix example (from J5 repo)

---
+++ matrix.js
@@ -0,0 +1,67 @@
+// https://github.com/rwaldron/johnny-five/blob/master/docs/led-matrix.md
+
+// VCC -> +5V
+// GND -> GND
+
+const five = require('johnny-five');
+
+new five.Board().on("ready", function() {
+
+    const heart = [
+        "01100110",
+        "10011001",
+        "10000001",
+        "10000001",
+        "01000010",
+        "00100100",
+        "00011000",
+        "00000000"
+    ];
+
+    const square = [
+        "11111111",
+        "10000001",
+        "10000001",
+        "10000001",
+        "10000001",
+        "10000001",
+        "10000001",
+        "11111111"
+    ];
+
+    const matrix = new five.Led.Matrix({
+        pins: {
+            data: 2,    // DIN
+            clock: 3,   // CLK
+            cs: 4       // CS
+        }
+    });
+
+    matrix.on();
+
+    let msg = "johnny-five".split("");
+
+    // Display each letter for 1 second
+    function next() {
+        var c;
+
+        if (c = msg.shift()) {
+            matrix.draw(c);
+            setTimeout(next, 1000);
+        }
+    }
+
+    next();
+
+    this.repl.inject({
+        matrix: matrix,
+        // Type "heart()" in the REPL to
+        // display a heart!
+        heart: function() {
+            matrix.draw(heart);
+        },
+        square: function() {
+            matrix.draw(square);
+        }
+    });
+});
